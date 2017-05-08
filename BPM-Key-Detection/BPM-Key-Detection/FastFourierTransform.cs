d2e9414cadb20e29516525f1e1fb83440dc9563a
Martin Eldevig Olsen <meol16@student.aau.dk>
2017-05-08 13:31:44 +0200

    FFT med komplekse tal

--- BPM-Key-Detection/BPM-Key-Detection/FastFourierTransform.cs
+++ BPM-Key-Detection/BPM-Key-Detection/FastFourierTransform.cs
@@ -36,5 +36,22 @@ namespace testapp
             }
             return output;
         }
+
+        public static Complex[] ComplexFFT(Complex[] input)
+        {
+            int length = input.Length;
+            Exocortex.DSP.Complex[] temp = new Exocortex.DSP.Complex[length];
+            for (int i = 0; i < length; i++)
+            {
+                temp[i] = new Exocortex.DSP.Complex(input[i].Real, input[i].Imaginary);
+            }
+            Exocortex.DSP.Fourier.FFT(temp, length, Exocortex.DSP.FourierDirection.Forward);
+            Complex[] output = new Complex[length];
+            for (int i = 0; i < length; i++)
+            {
+                output[i] = new Complex(temp[i].Re, temp[i].Im);
+            }
+            return output;
+        }
     }
 }
