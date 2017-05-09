bb13428643893b8b2e7f6c0825b9f2ca6ef503ba
Martin Eldevig Olsen <meol16@student.aau.dk>
2017-05-09 18:23:09 +0200

    FFT metode

--- BPM-Key-Detection/BPM-Key-Detection/FastFourierTransform.cs
+++ BPM-Key-Detection/BPM-Key-Detection/FastFourierTransform.cs
@@ -53,5 +53,60 @@ namespace testapp
             }
             return output;
         }
+
+
+
+        public static int BitReverse(int n, int bits)
+        {
+            int reversedN = n;
+            int count = bits - 1;
+
+            n >>= 1;
+            while (n > 0)
+            {
+                reversedN = (reversedN << 1) | (n & 1);
+                count--;
+                n >>= 1;
+            }
+
+            return ((reversedN << count) & ((1 << bits) - 1));
+        }
+
+
+        public static void FFT(Complex[] buffer)
+        {
+
+            int bits = (int)Math.Log(buffer.Length, 2);
+            for (int j = 1; j < buffer.Length / 2; j++)
+            {
+
+                int swapPos = BitReverse(j, bits);
+                var temp = buffer[j];
+                buffer[j] = buffer[swapPos];
+                buffer[swapPos] = temp;
+            }
+
+            for (int N = 2; N <= buffer.Length; N <<= 1)
+            {
+                for (int i = 0; i < buffer.Length; i += N)
+                {
+                    for (int k = 0; k < N / 2; k++)
+                    {
+
+                        int evenIndex = i + k;
+                        int oddIndex = i + k + (N / 2);
+                        var even = buffer[evenIndex];
+                        var odd = buffer[oddIndex];
+
+                        double term = -2 * Math.PI * k / (double)N;
+                        Complex exp = new Complex(Math.Cos(term), Math.Sin(term)) * odd;
+
+                        buffer[evenIndex] = even + exp;
+                        buffer[oddIndex] = even - exp;
+
+                    }
+                }
+            }
+        }
     }
 }
