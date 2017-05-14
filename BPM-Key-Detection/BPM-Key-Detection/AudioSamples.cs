808d2dced82e0490573270c7635cae1e5c1c82d5
Martin Eldevig Olsen <meol16@student.aau.dk>
2017-05-14 21:00:18 +0200

    AudioSamples har filter på 10.000 hz

--- BPM-Key-Detection/BPM-Key-Detection/AudioSamples.cs
+++ BPM-Key-Detection/BPM-Key-Detection/AudioSamples.cs
@@ -16,13 +16,15 @@ namespace testapp
             SampleRate = File.WaveFormat.SampleRate;
             Channels = File.WaveFormat.Channels;
 
+            NAudio.Dsp.BiQuadFilter lowPassFilter = NAudio.Dsp.BiQuadFilter.LowPassFilter(SampleRate, 10000, 1);
+
             int floatLength = (int)(File.Length / 4);
             double[] output = new double[floatLength];
             float[] buffer = new float[floatLength];
             File.Read(buffer, 0, floatLength);
             for (int i = 0; i < floatLength; i++)
             {
-                output[i] = buffer[i];
+                output[i] = lowPassFilter.Transform(buffer[i]);
             }
             return output;
         }
